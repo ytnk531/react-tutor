0a88f32e572dfd2cc5d150d4b29468c1047f1292
yudai tanaka <ytnk531@gmail.com>
2018-06-24 13:25:24 +0900

    clean

--- src/SortPanel.js
+++ src/SortPanel.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import {
   Grid,
   Row,
+  Col,
   Button,
   ButtonGroup,
   FormControl,
@@ -32,29 +33,32 @@ const mapStateToElementsProps = state => state;
 const ElementsContainer = connect(mapStateToElementsProps)(Elements);
 
 const Controllers = ({ dispatch, size }) => (
-  <div className="insertionSortController">
-    <div className="col-xs-2">
-      <ControlLabel>Number Of Elements</ControlLabel>
-      <FormControl
-        type="text"
-        onChange={e => dispatch({ type: 'CHANGESIZE', size: e.target.value })}
-        value={size}
-      />
-    </div>
-    <div>
+  <div className="insertionSortController center-block text-center">
+    <Row>
+      <Col xs={2} xsOffset={5}>
+        <ControlLabel>Number Of Elements</ControlLabel>
+        <FormControl
+          type="text"
+          onChange={e => dispatch({ type: 'CHANGESIZE', size: e.target.value })}
+          value={size}
+          bsSize="10em"
+        />
+      </Col>
+    </Row>
+    <Row>
       <ButtonGroup>
         <Button onClick={() => dispatch({ type: 'MINI' })}>Mini step</Button>
         <Button onClick={() => dispatch({ type: 'NEXT' })}>Next step</Button>
         <Button onClick={() => dispatch({ type: 'GO' })}>Sort All</Button>
       </ButtonGroup>
-    </div>
+    </Row>
 
-    <div>
+    <Row className="center-block text-center">
       <ButtonGroup>
         <Button onClick={() => dispatch({ type: 'RESET' })}>Reset</Button>
         <Button onClick={() => dispatch({ type: 'NEW' })}>New</Button>
       </ButtonGroup>
-    </div>
+    </Row>
   </div>
 );
 const mapStateToControllerProps = state => state;
@@ -63,15 +67,11 @@ const ControllersContainer = connect(mapStateToControllerProps)(Controllers);
 const SortPanel = () => (
   <div className="SortPanel">
     <Grid>
-      <Row>
-        <h1>Bubble Sort</h1>
-      </Row>
+      <h1 className="text-center">Bubble Sort</h1>
       <Row>
         <ElementsContainer />
       </Row>
-      <Row>
-        <ControllersContainer />
-      </Row>
+      <ControllersContainer />
     </Grid>
   </div>
 );
